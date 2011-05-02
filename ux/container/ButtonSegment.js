b2a76b020536e0652ff2a77f50bd64fbecda43e3
harrydeluxe <hanek@harrydeluxe.de>
2011-05-02 12:39:11 +0200

    initial import

---
+++ ux/container/ButtonSegment.js
@@ -0,0 +1,41 @@
+/*
+ * Ext.ux.container.ButtonSegment Class
+ * 
+ * @author Harald Hanek (c) 2011
+ * 
+ * License: MIT (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+Ext.define('Ext.ux.container.ButtonSegment', {
+    extend: 'Ext.container.ButtonGroup',
+    alias: 'widget.buttonsegment',
+
+    baseCls: Ext.baseCSSPrefix + 'sbtn',
+
+    buttonCls: Ext.baseCSSPrefix + 'sbtn',
+    
+    unstyled: true,
+    
+    frame: false,
+    
+    onRender: function()
+	{
+		var me = this;
+		me.callParent(arguments);
+
+		me.activeItem = (me.activeItem + 1 > me.items.length) ? 0 : me.activeItem;
+
+		me.items.each(function(el, c)
+		{
+			if(me.items.length === 1)
+				el.cls = ' ' + me.buttonCls +'-single';
+			else
+				el.cls = c == 0 ? ' ' + me.buttonCls +'-first' : (c == me.items.length - 1 ? ' ' + me.buttonCls +'-last' : ' ' + me.buttonCls +'-item');
+		});
+
+	},
+
+    onBeforeAdd: function(component)
+    {
+    }
+});
