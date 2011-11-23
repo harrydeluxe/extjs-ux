77a167ead5f898508c7fe7458e805886961e24e1
harrydeluxe <hanek@harrydeluxe.de>
2011-11-24 00:59:15 +0100

    format source

--- ux/grid/feature/Tileview.js
+++ ux/grid/feature/Tileview.js
@@ -8,59 +8,56 @@
 
 Ext.define('Ext.ux.grid.feature.Tileview', {
 	extend: 'Ext.grid.feature.Feature',
-	alias: 'feature.tileview',	
-	metaTableTplOrig: null, // stores the original template   
-    viewMode: null,	
-    viewTpls: {},   
-    
-	metaTableTpl: ['{[this.openTableWrap()]}',
-	              '<table class="' + Ext.baseCSSPrefix + 'grid-table tileview" border="0" cellspacing="0" cellpadding="0" style="width: auto;">',
-	                   '<tbody class="dragselect">',
-	                   '{[this.openRows()]}',
-	                       '{row}',
-	                       '<tpl for="features">',
-	                           '{[this.embedFeature(values, parent, xindex, xcount)]}',
-	                       '</tpl>',
-	                   '{[this.closeRows()]}',
-	                   '</tbody>',
-	               '</table>',
-	               '{[this.closeTableWrap()]}'
-	               ],
+	alias: 'feature.tileview',
+	metaTableTplOrig: null, // stores the original template
+	viewMode: null,
+	viewTpls: {},
 
-    
+	metaTableTpl: ['{[this.openTableWrap()]}',
+		              '<table class="' + Ext.baseCSSPrefix + 'grid-table tileview" border="0" cellspacing="0" cellpadding="0" style="width: auto;">',
+		                   '<tbody class="dragselect">',
+		                   '{[this.openRows()]}',
+		                       '{row}',
+		                       '<tpl for="features">',
+		                           '{[this.embedFeature(values, parent, xindex, xcount)]}',
+		                       '</tpl>',
+		                   '{[this.closeRows()]}',
+		                   '</tbody>',
+		               '</table>',
+		               '{[this.closeTableWrap()]}'
+		               ],
+
 	getRowBody: function(values, viewMode)
 	{
 		if(this.viewTpls[viewMode])
 		{
 			return this.viewTpls[viewMode];
 		}
-	},	
-	
+	},
 
 	mutateMetaRowTpl: function(metaRowTpl)
 	{
 		if(this.view.chunker)
 		{
-			if(!this.metaTableTplOrig)			
+			if(!this.metaTableTplOrig)
 				this.metaTableTplOrig = this.view.chunker.metaTableTpl;
 
 			if(!this.viewMode || this.viewMode == 'default')
 				this.view.chunker.metaTableTpl = this.metaTableTplOrig;
 			else
-				this.view.chunker.metaTableTpl = this.metaTableTpl;		
+				this.view.chunker.metaTableTpl = this.metaTableTpl;
 		}
-		
+
 		if(this.viewMode && this.viewMode != 'default')
 		{
 			delete metaRowTpl[1];
 			delete metaRowTpl[3];
-			
+
 			metaRowTpl[2] = '{[this.getRowBody(values, this.viewMode)]}';
 			metaRowTpl[0] = metaRowTpl[0].replace(Ext.baseCSSPrefix + 'grid-row', Ext.baseCSSPrefix + 'grid-row tileview');
 		}
 	},
 
-	
 	getMetaRowTplFragments: function()
 	{
 		return {
@@ -70,13 +67,12 @@ Ext.define('Ext.ux.grid.feature.Tileview', {
 		};
 	},
 
-	
 	setView: function(mode)
 	{
 		if(this.viewMode != mode)
 		{
-			this.viewMode = mode;	
+			this.viewMode = mode;
 			this.view.refresh();
 		}
-	}  
+	}
 });
