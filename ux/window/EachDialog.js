022a311b5df10032395ff357a490d8e8ac2c2bd5
harrydeluxe <hanek@harrydeluxe.de>
2011-12-29 03:32:54 +0100

    format

--- ux/window/EachDialog.js
+++ ux/window/EachDialog.js
@@ -7,96 +7,90 @@
  */
 
 Ext.define('Ext.ux.window.EachDialog', {
-    extend: 'Ext.window.MessageBox',   
-    
-    initComponent: function()
-    {
-        var me = this;
-		
-        me.callParent();
-   
-        me.promptContainer.remove(me.msg);
-        delete me.msg;
-        
+	extend: 'Ext.window.MessageBox',
+
+	initComponent: function()
+	{
+		var me = this;
+
+		me.callParent();
+
+		me.promptContainer.remove(me.msg);
+		delete me.msg;
+
 		me.msg = Ext.create('Ext.container.Container', {
-            layout: {
-                type: 'anchor'
-            },          
-            
-            items: [
-				me.msgtext = Ext.create('Ext.Component', {
-				    autoEl: { tag: 'span' },
-				    cls: 'ext-mb-text'
-				}),
-				me.checkbox = Ext.create('Ext.form.field.Checkbox', {
-				    anchor: '100%',
-				    name      : 'topping',
-				    inputValue: '1'
-				})
-            ],
-           
-            update: function(v)
-            {
-            	me.msgtext.update(v);
-            },
-            
-            getWidth: function()
-            {
-            	return me.msgtext.getWidth();
-            }
-        });
-		me.promptContainer.add(me.msg);      
-    },
+			layout: {
+				type: 'anchor'
+			},
 
-    
-    eachdialog: function(cfg, msg, fn, scope, multiline, value)
-    {
-        
-    	if(Ext.isString(cfg))
-    	{
-            cfg = {
-                prompt: true,
-                title: cfg,
-                minWidth: this.minPromptWidth,
-                msg: msg,
-                buttons: this.OKCANCEL,
-                callback: fn,
-                scope: scope,
-                multiline: multiline,
-                value: value
-            };
-        }
-        return this.show(cfg);
-    },
+			items: [ me.msgtext = Ext.create('Ext.Component', {
+				autoEl: {
+					tag: 'span'
+				},
+				cls: 'ext-mb-text'
+			}), me.checkbox = Ext.create('Ext.form.field.Checkbox', {
+				anchor: '100%',
+				name: 'topping',
+				inputValue: '1'
+			}) ],
 
-    
-    reconfigure: function(cfg)
-    {
-        var me = this;
-        
-        if(cfg.label)
-        {
-            me.checkbox.boxLabel = cfg.label;
-        }
-        me.callParent([cfg]);
-    },
+			update: function(v)
+			{
+				me.msgtext.update(v);
+			},
 
-    
-    btnCallback: function(btn)
-    {
-        var me = this,
-            value,
-            field;
+			getWidth: function()
+			{
+				return me.msgtext.getWidth();
+			}
+		});
+		me.promptContainer.add(me.msg);
+	},
 
-        field = me.checkbox;
-        value = field.getValue();
-        field.reset();
+	eachdialog: function(cfg, msg, fn, scope, multiline, value)
+	{
+		if(Ext.isString(cfg))
+		{
+			cfg = {
+				prompt: true,
+				title: cfg,
+				minWidth: this.minPromptWidth,
+				msg: msg,
+				buttons: this.OKCANCEL,
+				callback: fn,
+				scope: scope,
+				multiline: multiline,
+				value: value
+			};
+		}
+		return this.show(cfg);
+	},
 
-        btn.blur();
-        me.hide();
-        me.userCallback(btn.itemId, value, me.cfg);
-    }
+	reconfigure: function(cfg)
+	{
+		var me = this;
 
-}, function() {
+		if(cfg.label)
+		{
+			me.checkbox.boxLabel = cfg.label;
+		}
+		me.callParent([ cfg ]);
+	},
+
+	btnCallback: function(btn)
+	{
+		var me = this, value, field;
+
+		field = me.checkbox;
+		value = field.getValue();
+		field.reset();
+
+		btn.blur();
+		me.hide();
+		me.userCallback(btn.itemId, value, me.cfg);
+	}
+
+}, function()
+{
 	Ext.ux.window.EachDialog = new this();
 });
