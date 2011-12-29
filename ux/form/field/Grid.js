70f230a13d539e9b190fafb678aa88700302c75a
harrydeluxe <hanek@harrydeluxe.de>
2011-12-29 03:29:31 +0100

    Add Grid as field to form

---
+++ ux/form/field/Grid.js
@@ -0,0 +1,75 @@
+/**
+ * @class Ext.ux.form.field.Grid
+ * 
+ * The Initial Developer of the Original Code is tz (atian25@qq.com)
+ * @see http://www.sencha.com/forum/showthread.php?131482-Ext.ux.RowEditing-add-some-usefull-features
+ * 
+ * @author Harald Hanek (c) 2011
+ * @license MIT (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+Ext.define('Ext.ux.form.field.Grid', {
+
+	mixins: {
+		// labelable: 'Ext.form.Labelable',
+		field: 'Ext.form.field.Field'
+	},
+
+	setValue: function(value)
+	{
+		var me = this;
+
+		me.store.loadData(value);
+		me.value = value;
+
+		me.checkChange();
+		return me;
+	},
+
+	getSubmitData: function()
+	{
+		var me = this, data = null;
+		if(!me.disabled && me.submitValue && !me.isFileUpload())
+		{
+			data = {};
+			data[me.getName()] = '' + Ext.encode(me.getValue());
+		}
+		return data;
+	},
+
+	getValue: function()
+	{
+		var me = this;
+		return me.getData();
+	},
+
+	getData: function()
+	{
+		var me = this, data = [], i, r, key;
+
+		for(i = 0; i < me.store.data.items.length; i++)
+		{
+			r = me.store.data.items[i].data;
+
+			data[i] = {};
+
+			for(key in r)
+			{
+				data[i][key] = r[key];
+			}
+		}
+		return data;
+	},
+
+	reset: function()
+	{
+		var me = this;
+
+		me.store.removeAll();
+
+		me.setValue(me.originalValue);
+		me.clearInvalid();
+		// delete here so we reset back to the original state
+		delete me.wasValid;
+	}
+});
