f5da15c4e4cc86be516b360dde52e442e6c6d605
harrydeluxe <hanek@harrydeluxe.de>
2012-05-03 00:26:49 +0200

    Update ace to current revision

---
+++ example/aceeditor/simple_2.js
@@ -0,0 +1,24 @@
+Ext.Loader.setConfig({
+	enabled: true,
+	paths: {
+		'Ext.ux.aceeditor': '../../ux/aceeditor'
+	}
+});
+
+Ext.require([ 'Ext.ux.aceeditor.Panel' ]);
+
+Ext.onReady(function()
+{
+	new Ext.Viewport({
+		layout: 'border',
+		items: [ {
+			region: 'center',
+			xtype: 'AceEditor',
+			unstyled: true,
+			theme: 'twilight',
+			fontSize: '13px',
+			contentEl: 'pre_1',
+			parser: 'liquid'
+		} ]
+	});
+});
