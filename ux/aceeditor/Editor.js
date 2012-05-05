637117946daeef6ed17c9a85e6f114efcc14dc6b
harrydeluxe <hanek@harrydeluxe.de>
2012-05-05 20:35:13 +0200

    Add more config options

--- ux/aceeditor/Editor.js
+++ ux/aceeditor/Editor.js
@@ -5,89 +5,102 @@
  * @author Harald Hanek (c) 2011-2012
  * @license http://harrydeluxe.mit-license.org
  */
-
 Ext.define('Ext.ux.aceeditor.Editor', {
-	path: '',
-	sourceCode: '',
-	fontSize: '12px',
-	theme: 'clouds',
-	printMargin: false,
-	highlightActiveLine: true,
-	tabSize: 4,
-	useSoftTabs: false,
-	showInvisible: false,
-	useWrapMode: false,
-
-	initEditor: function()
-	{
-		var me = this;
-
-		me.editor = ace.edit(me.editorId);
-		me.setMode(me.parser);
-		me.setTheme(me.theme);
-		me.editor.getSession().setUseWrapMode(me.useWrapMode);
-		me.editor.setShowInvisibles(me.showInvisible);
-		me.setFontSize(me.fontSize);
-		me.editor.setShowPrintMargin(me.printMargin);
-		me.editor.setHighlightActiveLine(me.highlightActiveLine);
-		me.getSession().setTabSize(me.tabSize);
-		me.getSession().setUseSoftTabs(me.useSoftTabs);
-		me.setValue(me.sourceCode);
-		me.editor.getSession().on('change', function()
-		{
-			me.fireEvent('change', me);
-		}, me);
-
-		me.editor.focus();
-	},
-
-	getEditor: function()
-	{
-		return this.editor;
-	},
-
-	getSession: function()
-	{
-		return this.editor.getSession();
-	},
-
-	getTheme: function()
-	{
-		this.editor.getTheme();
-	},
-
-	setTheme: function(name)
-	{
-		this.editor.setTheme("ace/theme/" + name);
-	},
-
-	setMode: function(mode)
-	{
-		this.getSession().setMode("ace/mode/" + mode);
-	},
-
-	getValue: function()
-	{
-		this.editor.getSession().getValue();
-	},
-
-	setValue: function(value)
-	{
-		this.editor.getSession().setValue(value);
-	},
-
-	setFontSize: function(value)
-	{
-		this.editor.setFontSize(value);
-	},
-
-	undo: function()
-	{
-		this.editor.undo();
-	},
-
-	redo: function()
-	{
-		this.editor.redo();
-	}
+    path: '',
+    sourceCode: '',
+    autofocus: true,
+    fontSize: '12px',
+    theme: 'clouds',
+    printMargin: false,
+    printMarginColumn: 80,
+    highlightActiveLine: true,
+    highlightGutterLine: true,
+    highlightSelectedWord: true,
+    showGutter: true,
+    fullLineSelection: true,
+    tabSize: 4,
+    useSoftTabs: false,
+    showInvisible: false,
+    useWrapMode: false,
+    codeFolding: true,
+    
+    initEditor: function()
+    {
+        var me = this;
+        me.editor = ace.edit(me.editorId);
+        me.setMode(me.parser);
+        me.setTheme(me.theme);
+        me.editor.getSession().setUseWrapMode(me.useWrapMode);
+        me.editor.setShowFoldWidgets(me.codeFolding);
+        me.editor.setShowInvisibles(me.showInvisible);
+        me.editor.setHighlightGutterLine(me.highlightGutterLine);
+        me.editor.setHighlightSelectedWord(me.highlightSelectedWord);
+        me.editor.renderer.setShowGutter(me.showGutter);
+        me.setFontSize(me.fontSize);
+        me.editor.setShowPrintMargin(me.printMargin);
+        me.editor.setPrintMarginColumn(me.printMarginColumn);
+        me.editor.setHighlightActiveLine(me.highlightActiveLine);
+        me.getSession().setTabSize(me.tabSize);
+        me.getSession().setUseSoftTabs(me.useSoftTabs);
+        me.setValue(me.sourceCode);
+        me.editor.getSession().on('change', function()
+        {
+            me.fireEvent('change', me);
+        }, me);
+        
+        if(me.autofocus)
+            me.editor.focus();
+        else
+            me.editor.blur();
+    },
+    
+    getEditor: function()
+    {
+        return this.editor;
+    },
+    
+    getSession: function()
+    {
+        return this.editor.getSession();
+    },
+    
+    getTheme: function()
+    {
+        this.editor.getTheme();
+    },
+    
+    setTheme: function(name)
+    {
+        this.editor.setTheme("ace/theme/" + name);
+    },
+    
+    setMode: function(mode)
+    {
+        this.getSession().setMode("ace/mode/" + mode);
+    },
+    
+    getValue: function()
+    {
+        this.editor.getSession().getValue();
+    },
+    
+    setValue: function(value)
+    {
+        this.editor.getSession().setValue(value);
+    },
+    
+    setFontSize: function(value)
+    {
+        this.editor.setFontSize(value);
+    },
+    
+    undo: function()
+    {
+        this.editor.undo();
+    },
+    
+    redo: function()
+    {
+        this.editor.redo();
+    }
 });
