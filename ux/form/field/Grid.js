f42456edc8fe4a5729b732ae30d4f24803380067
harrydeluxe <hanek@harrydeluxe.de>
2012-05-16 02:01:54 +0200

    clean up

--- ux/form/field/Grid.js
+++ ux/form/field/Grid.js
@@ -1,115 +1,117 @@
 /**
  * @class Ext.ux.form.field.Grid
  * 
- * @author Harald Hanek (c) 2011
- * @license MIT (http://www.opensource.org/licenses/mit-license.php)
+ * @author Harald Hanek (c) 2011-2012
+ * @license http://harrydeluxe.mit-license.org
  */
 
 Ext.define('Ext.ux.form.field.Grid', {
 
-	mixins: {
-		field: 'Ext.form.field.Field'
-	},
+    mixins: {
+        field: 'Ext.form.field.Field'
+    },
 
-	initField: function()
-	{
-		var me = this;
+    initField: function()
+    {
+        var me = this;
+        
+        me.addEvents(
+        /**
+         * @event change Fires when a user-initiated change is detected in the
+         *        value of the field.
+         * @param {Ext.form.field.Field} this
+         * @param {Object} newValue The new value
+         * @param {Object} oldValue The original value
+         */
+        'change',
+        /**
+         * @event validitychange Fires when a change in the field's validity is
+         *        detected.
+         * @param {Ext.form.field.Field} this
+         * @param {Boolean} isValid Whether or not the field is now valid
+         */
+        'validitychange',
+        /**
+         * @event dirtychange Fires when a change in the field's
+         *        {@link #isDirty} state is detected.
+         * @param {Ext.form.field.Field} this
+         * @param {Boolean} isDirty Whether or not the field is now dirty
+         */
+        'dirtychange');
 
-		me.addEvents(
-		/**
-		 * @event change Fires when a user-initiated change is detected in the
-		 *        value of the field.
-		 * @param {Ext.form.field.Field} this
-		 * @param {Object} newValue The new value
-		 * @param {Object} oldValue The original value
-		 */
-		'change',
-		/**
-		 * @event validitychange Fires when a change in the field's validity is
-		 *        detected.
-		 * @param {Ext.form.field.Field} this
-		 * @param {Boolean} isValid Whether or not the field is now valid
-		 */
-		'validitychange',
-		/**
-		 * @event dirtychange Fires when a change in the field's
-		 *        {@link #isDirty} state is detected.
-		 * @param {Ext.form.field.Field} this
-		 * @param {Boolean} isDirty Whether or not the field is now dirty
-		 */
-		'dirtychange');
+        me.initValue();
 
-		me.initValue();
+        me.store.addListener('add', me.checkDirty, me);
+        me.store.addListener('update', me.checkDirty, me);
+        me.store.addListener('remove', me.checkDirty, me);
+    },
 
-		me.store.addListener('add', me.checkDirty, me);
-		me.store.addListener('update', me.checkDirty, me);
-		me.store.addListener('remove', me.checkDirty, me);
-	},
+    isDirty: function()
+    {
+        var me = this;
+        return !me.disabled && !me.isEqual(Ext.encode(me.getValue()), Ext.encode(me.originalValue));
+    },
 
-	isDirty: function()
-	{
-		var me = this;
-		return !me.disabled && !me.isEqual(Ext.encode(me.getValue()), Ext.encode(me.originalValue));
-	},
+    setValue: function(value)
+    {
+        var me = this;
 
-	setValue: function(value)
-	{
-		var me = this;
+        if(value === null || value === undefined)
+        {
+            value = [];
+        }
 
-		if(value === null || value === undefined)
-		{
-			value = [];
-		}
-		me.store.loadData(value);
-		me.value = value;
-		me.checkChange();
-		return me;
-	},
+        me.store.loadData(value);
+        me.value = value;
+        me.checkChange();
+        return me;
+    },
 
-	getSubmitData: function()
-	{
-		var me = this, data = null;
-		if(!me.disabled && me.submitValue && !me.isFileUpload())
-		{
-			data = {};
-			data[me.getName()] = '' + Ext.encode(me.getValue());
-		}
-		return data;
-	},
+    getSubmitData: function()
+    {
+        var me = this, data = null;
+        if(!me.disabled && me.submitValue && !me.isFileUpload())
+        {
+            data = {};
+            data[me.getName()] = '' + Ext.encode(me.getValue());
+        }
+        return data;
+    },
 
-	getValue: function()
-	{
-		var me = this;
-		return me.getData();
-	},
+    getValue: function()
+    {
+        var me = this;
+        return me.getData();
+    },
 
-	getData: function()
-	{
-		var me = this, data = [], i, r, key;
+    getData: function()
+    {
+        var me = this,
+            data = [], i, r, key;
 
-		for(i = 0; i < me.store.data.items.length; i++)
-		{
-			r = me.store.data.items[i].data;
+        for(i = 0; i < me.store.data.items.length; i++)
+        {
+            r = me.store.data.items[i].data;
 
-			data[i] = {};
+            data[i] = {};
 
-			for(key in r)
-			{
-				data[i][key] = r[key];
-			}
-		}
-		return data;
-	},
+            for(key in r)
+            {
+                data[i][key] = r[key];
+            }
+        }
+        return data;
+    },
 
-	reset: function()
-	{
-		var me = this;
+    reset: function()
+    {
+        var me = this;
 
-		me.store.removeAll();
+        me.store.removeAll();
 
-		me.setValue(me.originalValue);
-		me.clearInvalid();
-		// delete here so we reset back to the original state
-		delete me.wasValid;
-	}
+        me.setValue(me.originalValue);
+        me.clearInvalid();
+        // delete here so we reset back to the original state
+        delete me.wasValid;
+    }
 });
